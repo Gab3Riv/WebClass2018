9d1eb5ec655a78c05702dd017e2a9a20494b1865
Gab3Riv <gabrielrivera@lewisu.edu>
2018-10-28 20:34:58 -0500

    adding my own projects

--- personalForFun/main.js
+++ personalForFun/main.js
@@ -1,12 +1,20 @@
+var randomOne = function(){
+    var randomNum = Math.floor(Math.random()*10);
+    document.getElementById("firstNum").value = randomNum;
+}
+var randomTwo = function(){
+    var randomNum = Math.floor(Math.random()*10);
+    document.getElementById("secondNum").value = randomNum;
+}
+var randomThree = function(){
+    var randomNum = Math.floor(Math.random()*10);
+    document.getElementById("thirdNum").value = randomNum;
+}
 var raffle = function(){
     var i = 0;
     var ran1, ran2, ran3, ran4;
-    var index = 2;
-    ran1 = Math.floor(Math.random()*10);
-    ran2 = Math.floor(Math.random()*10);
-    ran3 = Math.floor(Math.random()*10);
-    ran4 = Math.floor(Math.random()*10);
-    document.getElementById("result").innerHTML = "1. " + ran1 + " " + ran2 + " " + ran3 + " Fireball: " + ran4 + " <br>";
+    var index = 1;
+    document.getElementById("result").innerHTML = "";
     while(i < 100){
         i+=1;
         ran1 = Math.floor(Math.random()*10);
