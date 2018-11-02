c4864998396abbd738463d1d69c8eee6d591b4f1
Gab3Riv <gabrielrivera@lewisu.edu>
2018-11-02 15:53:59 -0500

    Adding fun updates

--- personalForFun/virtualMoneyLogic.js
+++ personalForFun/virtualMoneyLogic.js
@@ -2,6 +2,12 @@ var balance = 0;
 var combinations = 1;
 var elementArray = ["drawingsDisplayOne","drawingsDisplayTwo","drawingsDisplayThree"];
 var pickThreeID = ["pickThreeOne","pickThreeTwo","pickThreeThree"];
+var straightID = ["straightOne","straightTwo","straightThree"];
+var boxID = ["boxOne","boxTwo","boxThree"];
+var straightboxID = ["straightBoxOne","straightBoxTwo","straightBoxThree"];
+var comboID = ["comboOne","comboTwo","comboThree"];
+var frontPairID = ["frontPairOne","frontPairTwo","frontPairThree"];
+var backPairID = ["backPairOne","backPairTwo","backPairThree"];
 var submitBalance = function(){
     balance += parseInt(document.getElementById("balance").value);
     document.getElementById("balance").value = 0;
@@ -22,7 +28,17 @@ var submitCombinations = function(){
     for(var i=0; i < 3; i+=1){
         document.getElementById(elementArray[i]).innerHTML = "";
         for(var ii=0; ii < combinations; ii+=1){
-            document.getElementById(elementArray[ii]).innerHTML = "<input id='" + pickThreeID[ii] +"'value='000'></input>";
+            document.getElementById(elementArray[ii]).innerHTML = "<input id='" + pickThreeID[ii] +"'value='000'></input>" +
+                '<input type="checkbox" name="fireball" value="fireball">Fireball<br>' +
+                '<form action="/action_page.php">' +
+                '<input type="checkbox" name="Straight" value="straight">Straight<br>' +
+                '<input type="checkbox" name="Box" value="box">Box<br>' +
+                '<input type="checkbox" name="StraightBox" value="straightBox">Straight/Box<br>' +
+                '<input type="checkbox" name="Combo" value="combo">Combo<br>' +
+                '<input type="checkbox" name="Front Pair" value="frontPair">Front Pair<br>' +
+                '<input type="checkbox" name="Back Pair" value="backPair">Back Pair<br>' +
+                //<input type="submit" value="Submit">
+                '</form>';
         }
     }
 }
