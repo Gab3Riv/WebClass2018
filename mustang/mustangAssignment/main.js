9afc463baff2e4416311ca4dd7acfca37fee7893
Gab3Riv <gabrielrivera@lewisu.edu>
2018-11-04 22:40:24 -0600

    Updated mustang lite

--- mustang/mustangAssignment/main.js
+++ mustang/mustangAssignment/main.js
@@ -1,33 +1,29 @@
 var contactURLArray = [];
+var contactsArray = [];
 
 var loadIndex = function(){
-    console.log("working");
-
     var indexRequest = new XMLHttpRequest();
     indexRequest.open("GET", "https://mustang-index.azurewebsites.net/index.json");
     indexRequest.onload = function(){
-        console.log("Got Index From Web " + indexRequest.responseText);
         var contactIndex = JSON.parse(indexRequest.responseText);
-        console.log(contactIndex);
         for(var i = 0; i<contactIndex.length; i+=1){
             contactURLArray.push(contactIndex[i].ContactURL);
         }
-        console.log(contactURLArray);
         document.getElementById("data").innerHTML = "Index: " + indexRequest.responseText;
     }
     indexRequest.send();
-    console.log("After open");
 }
-
 var loadContacts = function(){
     document.getElementById("contacts").innerHTML = "Contacts: ";
     var indexRequest = new XMLHttpRequest();
-    indexRequest.open("GET", JSON.stringify(contactURLArray[23]), false);
-    indexRequest.onload = function(){
-        console.log("This is from Load Contacts: " + indexRequest.responseText);
-        document.getElementById("contacts").innerHTML += indexRequest.responseText;
+    for(var i=0;i<contactURLArray.length;i+=1){
+        indexRequest.open("GET", contactURLArray[i], false);
+        indexRequest.onload = function(){
+            document.getElementById("contacts").innerHTML += "<br><div class='cards'>" + indexRequest.responseText + "</div><br>";
+            contactsArray.push(JSON.parse(indexRequest.response));
+        }
+        indexRequest.send();
     }
-    indexRequest.send();
 }
 /*
 var loadContacts = function(){
