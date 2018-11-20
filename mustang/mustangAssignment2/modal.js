1288d7b8433611d03f13156c08bf8ca65690181c
Gab3Riv <gabrielrivera@lewisu.edu>
2018-11-20 01:22:40 -0600

    Updating Mustang v2

---
+++ mustang/mustangAssignment2/modal.js
@@ -0,0 +1,20 @@
+var add = {
+    open: function(){
+        var modal = document.getElementById('modalID');
+        modal.style.display = "block";
+    },
+    close: function(){
+        var modal = document.getElementById('modalID');
+        modal.style.display = "none";
+    }
+}
+var remove = {
+    open: function(){
+        var modal = document.getElementById('modalremoveID');
+        modal.style.display = "block";
+    },
+    close: function(){
+        var modal = document.getElementById('modalremoveID');
+        modal.style.display = "none";
+    }
+}
